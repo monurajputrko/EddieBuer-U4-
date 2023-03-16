d07ffabff4a2c69fda7ca81f25a7a8993fccce2e
Anas <anasrashidshah@hmail.com>
2023-03-16 17:24:13 +0530

    signIn/signUp completed

---
+++ login.js
@@ -0,0 +1,8 @@
+// const username = localStorage.getItem("username");
+// document.getElementById("userNameDisplay").textContent = username;
+
+// function userNameDisplayFun() {
+//     const username = localStorage.getItem("username");
+//     document.getElementById("userNameDisplay").textContent = username;
+// }
+
